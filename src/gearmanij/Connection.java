00f93e039ad7199fb897ce9ae6cde71935114e3e
Eric Herman <eric@mysql.com>
2009-05-04 17:22:25 +0200

    add missing file

---
+++ src/gearmanij/Connection.java
@@ -0,0 +1,33 @@
+/*
+ * Copyright (C) 2009 by Eric Herman <eric@freesa.org>
+ * Copyright (C) 2009 by Robert Stewart
+ * Use and distribution licensed under the 
+ * GNU Lesser General Public License (LGPL) version 2.1.
+ * See the COPYING file in the parent directory for full text.
+ */
+package gearmanij;
+
+import gearmanij.util.RuntimeIOException;
+
+import java.util.List;
+import java.util.Map;
+
+public interface Connection {
+
+	void open();
+
+	void close();
+
+	void write(Packet request);
+
+	/**
+	 * Reads from socket and constructs a Packet.
+	 * 
+	 * @return
+	 * @throws RuntimeIOException
+	 */
+	Packet readPacket();
+
+	Map<String, List<String>> textMode(List<String> commands);
+
+}
