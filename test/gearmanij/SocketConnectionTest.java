ccc0534254df74860e1a8bb8063802d8147b01ce
Eric Herman <eric@mysql.com>
2009-05-05 13:13:39 +0200

    reorganize test to fail during the setUp() if the external dependencies are not met

--- test/gearmanij/SocketConnectionTest.java
+++ test/gearmanij/SocketConnectionTest.java
@@ -9,11 +9,11 @@ package gearmanij;
 
 import static org.junit.Assert.assertTrue;
 import gearmanij.util.ByteUtils;
-import gearmanij.util.RuntimeIOException;
 import gearmanij.util.TestUtil;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.Map;
 
 import org.junit.After;
 import org.junit.Before;
@@ -26,11 +26,7 @@ public class SocketConnectionTest {
   @Before
   public void setUp() {
     conn = new SocketConnection();
-    try {
-	  conn.open();
-    } catch (Exception e) {
-      e.printStackTrace();
-    }
+    conn.open();
   }
 
   @After
@@ -47,31 +43,24 @@ public class SocketConnectionTest {
   
   @Test
   public void testEcho() {
-    try {
-      byte[] textBytes = ByteUtils.toAsciiBytes("abc");
-      Packet request = new Packet(PacketMagic.REQ, PacketType.ECHO_REQ, textBytes);
-      conn.write(request);
-      Packet response = conn.readPacket();
-      assertTrue(response.getType() == PacketType.ECHO_RES);
-      // Response data is null terminated
-      assertTrue(textBytes.length == response.getDataSize());
-      // Assert data was "abc"
-      byte[] responseBytes = response.getData();
-      assertTrue(Arrays.equals(textBytes, responseBytes));
-    } catch (RuntimeIOException e) {
-      e.printStackTrace();
-    }
+    byte[] textBytes = ByteUtils.toAsciiBytes("abc");
+    Packet request = new Packet(PacketMagic.REQ, PacketType.ECHO_REQ, textBytes);
+    conn.write(request);
+    Packet response = conn.readPacket();
+    assertTrue(response.getType() == PacketType.ECHO_RES);
+    // Response data is null terminated
+    assertTrue(textBytes.length == response.getDataSize());
+    // Assert data was "abc"
+    byte[] responseBytes = response.getData();
+    assertTrue(Arrays.equals(textBytes, responseBytes));
   }
 
   @Test
   public void testTextMode() {
-    try {
-      // Verify connection
-      List<String> commands = Arrays.asList(Constants.TEXT_MODE_TEST_COMMANDS);
-      TestUtil.dump(System.out, conn.textMode(commands));
-    } catch (Exception e) {
-      e.printStackTrace();
-    }
+    List<String> commands = Arrays.asList(Constants.TEXT_MODE_TEST_COMMANDS);
+    // Verify connection
+    Map<String, List<String>> textModeResults = conn.textMode(commands);
+    TestUtil.dump(textModeResults);
   }
   
 }
