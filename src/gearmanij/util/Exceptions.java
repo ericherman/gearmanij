8957d6b0cb2d73b3cca1998a3666f6c1454169e0
Eric Herman <eric@mysql.com>
2009-05-22 14:21:45 +0200

    adding checked exception wrapper

---
+++ src/gearmanij/util/Exceptions.java
@@ -0,0 +1,32 @@
+/*
+ * Copyright (C) 2009 by Eric Herman <eric@freesa.org>
+ * Use and distribution licensed under the 
+ * GNU Lesser General Public License (LGPL) version 2.1.
+ * See the COPYING file in the parent directory for full text.
+ */
+package gearmanij.util;
+
+import java.io.IOException;
+import java.util.concurrent.Callable;
+
+public class Exceptions {
+
+  public static RuntimeException toRuntime(Exception e) {
+    if (e instanceof RuntimeException) {
+      return (RuntimeException) e;
+    }
+    if (e instanceof IOException) {
+      return new IORuntimeException((IOException) e);
+    }
+    return new RuntimeException(e);
+  }
+
+  public static <T> T call(Callable<T> callable) {
+    try {
+      return callable.call();
+    } catch (Exception e) {
+      throw toRuntime(e);
+    }
+  }
+
+}
---
+++ test/gearmanij/util/ExeptionsTest.java
@@ -0,0 +1,48 @@
+package gearmanij.util;
+
+import static org.junit.Assert.assertEquals;
+
+import java.io.IOException;
+import java.util.concurrent.Callable;
+
+import org.junit.Test;
+
+public class ExeptionsTest {
+
+  @Test
+  public void testToRuntime() {
+    RuntimeException e1 = new RuntimeException("x");
+    assertEquals(e1, Exceptions.toRuntime(e1));
+
+    Exception e2 = new Exception("y");
+    assertEquals(e2, Exceptions.toRuntime(e2).getCause());
+
+    IOException e3 = new IOException("z");
+    Exception x = Exceptions.toRuntime(e3);
+    assertEquals(IORuntimeException.class, x.getClass());
+
+  }
+
+  @Test
+  public void testCall() {
+    String foo = Exceptions.call(new Callable<String>() {
+      public String call() throws Exception {
+        return "foo";
+      }
+    });
+    assertEquals("foo", foo);
+
+    RuntimeException expected = null;
+    try {
+      Exceptions.call(new Callable<String>() {
+        public String call() throws Exception {
+          throw new Exception("a");
+        }
+      });
+    } catch (RuntimeException e) {
+      expected = e;
+    }
+    assertEquals("a", expected.getCause().getMessage());
+  }
+
+}
