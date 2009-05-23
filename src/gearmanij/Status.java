ef0f37392864cafd74d4cc6032b2dfbc78e87c07
Eric Herman <eric@mysql.com>
2009-05-23 12:55:28 -0500

    foundation for checking the progress on a job

---
+++ src/gearmanij/Status.java
@@ -0,0 +1,88 @@
+/*
+ * Copyright (C) 2009 by Eric Herman <eric@freesa.org>
+ * Copyright (C) 2009 by Eric Lambert
+ * Use and distribution licensed under the 
+ * GNU Lesser General Public License (LGPL) version 2.1.
+ * See the COPYING file in the parent directory for full text.
+ */
+package gearmanij;
+
+import gearmanij.util.ByteArrayBuffer;
+import gearmanij.util.ByteUtils;
+
+import java.util.List;
+
+public class Status {
+  private Connection conn;
+  private byte[] handle;
+  private long lastUpdate;
+
+  private boolean isRunning;
+  private boolean isKnown;
+  private long numerator;
+  private long denominator;
+
+  public Status(Connection conn) {
+    this.conn = conn;
+    this.handle = ByteUtils.EMPTY;
+  }
+
+  public void setHandle(byte[] handle) {
+    this.handle = handle;
+  }
+
+  public void update() {
+    conn.open();
+    try {
+      Packet req = new Packet(PacketMagic.REQ, PacketType.GET_STATUS, handle);
+      conn.write(req);
+      Packet resp = conn.read();
+      update(resp);
+    } finally {
+      conn.close();
+    }
+  }
+
+  public void update(Packet fromServer) {
+    ByteArrayBuffer buf = new ByteArrayBuffer(fromServer.getData());
+    byte[] pattern = new byte[] { ByteUtils.NULL };
+    int numberOfparts = 5;
+    List<byte[]> parts = buf.split(pattern, numberOfparts);
+
+    isKnown = parseBoolean(parts.get(1));
+    isRunning = parseBoolean(parts.get(2));
+    numerator = parseLong(parts.get(3));
+    denominator = parseLong(parts.get(4));
+
+    lastUpdate = System.currentTimeMillis();
+  }
+
+  private long parseLong(byte[] bytes) {
+    return Long.parseLong(ByteUtils.fromUTF8Bytes(bytes));
+  }
+
+  private boolean parseBoolean(byte[] bytes) {
+    return bytes[0] == '0' ? false : true;
+  }
+
+  public boolean isRunning() {
+    return isRunning;
+  }
+
+  public boolean isKnown() {
+    return isKnown;
+  }
+
+  public long getNumerator() {
+    return numerator;
+  }
+
+  public long getDenominator() {
+    return denominator;
+  }
+
+  public long getLastUpdate() {
+    return lastUpdate;
+  }
+
+}
---
+++ test/gearmanij/StatusCheckAcceptanceTest.java
@@ -0,0 +1,119 @@
+/*
+ * Copyright (C) 2009 by Eric Herman <eric@freesa.org>
+ * Use and distribution licensed under the 
+ * GNU Lesser General Public License (LGPL) version 2.1.
+ * See the COPYING file in the parent directory for full text.
+ */
+package gearmanij;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+
+import java.util.Arrays;
+
+import gearmanij.example.ReverseClient;
+import gearmanij.example.ReverseWorker;
+import gearmanij.util.ByteUtils;
+import gearmanij.util.TestUtil;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class StatusCheckAcceptanceTest {
+
+  private boolean jobFinished;
+  private String reversed;
+  private ReverseWorker worker;
+  private ReverseClient client;
+  private Status status;
+
+  @Before
+  public void setUp() {
+    jobFinished = false;
+    reversed = null;
+
+    SocketConnection c1 = new SocketConnection(host(), port());
+    client = new ReverseClient(c1);
+
+    SocketConnection c2 = new SocketConnection(host(), port());
+    worker = new ReverseWorker(c2);
+
+    SocketConnection c3 = new SocketConnection(host(), port());
+    status = new Status(c3);
+  }
+
+  @After
+  public void tearDown() {
+    reversed = null;
+    client = null;
+    worker.shutdown();
+    worker = null;
+    status = null;
+  }
+
+  // TODO not really @Deprecated, just make parameterizable
+  @Deprecated
+  private int port() {
+    return Constants.GEARMAN_DEFAULT_TCP_PORT;
+  }
+
+  // TODO not really @Deprecated, just make parameterizable
+  @Deprecated
+  private String host() {
+    return Constants.GEARMAN_DEFAULT_TCP_HOST;
+  }
+
+  @Test
+  public void testRoundTrip() throws Exception {
+    final String hello = "Hello";
+    final String olleh = "olleH";
+
+    Thread customerThread = TestUtil.startThread("Customer", new Runnable() {
+      public void run() {
+        reversed = client.reverse(hello);
+        jobFinished = true;
+      }
+    });
+
+    Thread workerThread = TestUtil.startThread("Worker", new Runnable() {
+      public void run() {
+        for (int i = 0; i < 10 && status.getLastUpdate() == 0; i++) {
+          TestUtil.sleep(200);
+        }
+        worker.start();
+      }
+    });
+
+    assertEquals(false, jobFinished);
+    assertEquals(0, status.getLastUpdate());
+
+    status.setHandle(getHandle(1000));
+    status.update();
+    assertTrue(status.getLastUpdate() > 0);
+
+    customerThread.join(2000);
+    worker.shutdown();
+    workerThread.join(1000);
+
+    assertTrue(jobFinished);
+    assertNotNull(reversed);
+    assertEquals(olleh.trim(), reversed.trim());
+    assertEquals(olleh, reversed);
+  }
+
+  private byte[] getHandle(long timeout) throws InterruptedException {
+    long start = System.currentTimeMillis();
+    byte[] handle = ByteUtils.EMPTY;
+    while (Arrays.equals(ByteUtils.EMPTY, handle)) {
+      handle = client.getHandle();
+      if (start + timeout > System.currentTimeMillis()) {
+        Thread.sleep(100);
+      }
+    }
+    assertTrue(!Arrays.equals(ByteUtils.EMPTY, handle));
+    return handle;
+  }
+
+}
